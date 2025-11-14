f7be08a8166dbe089ad0ba486c47fa80b1934041
Estuss <skorpion0521@gmail.com>
2025-11-14 18:45:19 +0700

    Исправил ошибку с импортом

--- backend/app/mailing/send_verification_email.py
+++ backend/app/mailing/send_verification_email.py
@@ -1,9 +1,9 @@
 from app.mailing.send_email import send_email
-from app.core.secuirity import create_access_token
+from app.core.secuirity import create_token
 
 
 async def send_verification_email(email: str):
-    registration_token = create_access_token({"email": email})
+    registration_token = create_token({"email": email})
     activation_url = f"/api/auth/confirm-email?token={registration_token}"
     await send_email(
         recipient_email=email,
