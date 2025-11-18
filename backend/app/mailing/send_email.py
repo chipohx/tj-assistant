98a2f96fa1e5405a0f917663baba076e35a587d5
Estuss <skorpion0521@gmail.com>
2025-11-18 21:34:08 +0700

    Закончена работа над регистрацией
    - Добавлен html шаблон письма
    - Добавлен эндпоинт активации аккаунта
    - Чувствительные данные перенесены в .env (EMAIL_LOGIN, EMAIL_PASSWORD, SECRET_KEY)
    - Создан user.py, куда вынесен функционал использующийся в auth.py

--- backend/app/mailing/send_email.py
+++ backend/app/mailing/send_email.py
@@ -1,15 +1,27 @@
+import aiosmtplib, os
+from dotenv import load_dotenv
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import aiosmtplib
 
+
+load_dotenv()
 host = "smtp.gmail.com"
-email = "skorpion0521@gmail.com"
-password = "fmsvprcclmpvcxzt"
+email = os.getenv("EMAIL_LOGIN")
+password = os.getenv("EMAIL_PASSWORD")
 
 
 async def send_email(
-    recipient_email: str, subject: str, plain_content: str, html_content: str = ""
+    recipient_email: str, subject: str, plain_content: str = "", html_content: str = ""
 ):
+    """Отправляет письмо на почту указанную в recipient_email
+    subject - заголовок письма
+    plain_content - текст письма
+    html_content - html документ"""
+
+    if not email or not password:
+        raise Exception("Email or password not specified in .env")
+
     message = MIMEMultipart("alternative")
     message["From"] = email
     message["To"] = recipient_email
@@ -22,6 +34,15 @@ async def send_email(
         html_message = MIMEText(html_content, "html", "utf-8")
         message.attach(html_message)
 
-    await aiosmtplib.send(
-        message, hostname=host, port=587, username=email, password=password
-    )
+    try:
+        await aiosmtplib.send(
+            message, hostname=host, port=587, username=email, password=password
+        )
+    except aiosmtplib.SMTPConnectError:
+        print("Authentication error")
+    except aiosmtplib.SMTPAuthenticationError:
+        print("Authentication error")
+    except aiosmtplib.SMTPTimeoutError:
+        print("Timeout error")
+    except aiosmtplib.SMTPException as e:
+        print(f"Email sending error: {e}")
