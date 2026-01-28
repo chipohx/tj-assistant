f16d998bb3b73bff7db23bbcb2e1d40c0c678cc5
Estuss <skorpion0521@gmail.com>
2026-01-28 21:03:42 +0700

    Поправил исправления

--- backend/app/api/endpoints/chat.py
+++ backend/app/api/endpoints/chat.py
@@ -44,16 +44,6 @@ async def send_message(
 
     chat_id = None
     if not request.chat_id:
-<<<<<<< Updated upstream
-        chat_created = await create_chat(db, request.content[:30], user)
-    chat_id = chat_created if chat_created else request.chat_id
-
-    await create_message(db, request.content, Role.USER, chat_id)
-
-    response_content = await request_llm_response(request.content)
-    if response_content:
-        await create_message(db, response_content, Role.SYSTEM, chat_id)
-=======
         chat_id = await create_chat(db, request.content[:30], user)
     else:
         chat_id = request.chat_id
@@ -61,16 +51,15 @@ async def send_message(
     await create_message(db, request.content, Role.USER, chat_id)
 
     # response_content = await request_llm_response(request.content)
->>>>>>> Stashed changes
 
     response_content = (
         f"Это тестовый ответ от ассистента на ваш запрос: '{request.content}'."
         "ML-сервис находится в разработке и будет подключен позже."
     )
 
-<<<<<<< Updated upstream
-=======
-    assistant_message_id = create_message(db, response_content, Role.SYSTEM, chat_id)
+    assistant_message_id = await create_message(
+        db, response_content, Role.SYSTEM, chat_id
+    )
 
     return ChatResponse(
         message_id=assistant_message_id,
@@ -79,7 +68,6 @@ async def send_message(
         chat_created=chat_id,
     )
 
->>>>>>> Stashed changes
 
 @router.get("/chats")
 async def get_chats(
@@ -87,15 +75,6 @@ async def get_chats(
     db: AsyncSession = Depends(get_db),
 ):
     try:
-<<<<<<< Updated upstream
-        result = await db.scalars(
-            Select(Chat)
-            .filter(Chat.user_id == user.id)
-            .options(defer(Chat.user_id))
-            .order_by(Chat.created.asc())
-        )
-        chats = result.all()
-=======
         chats = (
             await db.scalars(
                 Select(Chat)
@@ -117,7 +96,6 @@ async def get_chats(
             ],
             "count": len(chats),
         }
->>>>>>> Stashed changes
     except Exception as e:
         print(f"Ошибка при попытке получить чаты: {e}")
         return {"items": [], "count": 0}
@@ -133,48 +111,6 @@ async def get_chat_sessions(
     limit: int = Query(20, ge=1, le=30),
     last_id: UUID | None = None,
 ):
-<<<<<<< Updated upstream
-    chat: Chat = await db.scalar(Select(Chat).filter(Chat.id == chat_id))
-
-    if chat.user_id != user.id:
-        raise HTTPException(status_code=401, detail="Access forbidden")
-
-    db_messages = []
-    try:
-        db_messages = (
-            await db.scalars(
-                Select(Message)
-                .filter(Message.chat_id == chat_id)
-                .order_by(Message.created.asc())
-                .limit(limit)
-            )
-        ).all()
-    except Exception as e:
-        print(f"Ошибка при попытке получить сообщения из чата {chat.title}: {e}")
-
-    print(db_messages)
-
-    response_data = {
-        "count": len(db_messages),
-        "next_id": db_messages[-1].id if db_messages else None,
-        "items": [msg.__dict__ for msg in db_messages],
-    }
-
-    return MessagesListResponse[MessageSchema].model_validate(response_data)
-
-
-# @router.post("/chat")
-# async def send_message(
-#     request: ChatRequest,
-#     user: Annotated[User, Depends(get_current_active_user)],
-#     db: AsyncSession = Depends(get_db),
-# ) -> ChatResponse:
-#     return ChatResponse(
-#         content="Привет",
-#         timestamp=datetime.now(),
-#         chat_created=UUID("12345678-1234-5678-1234-567812345678"),
-#     )
-=======
     try:
         chat: Chat = await db.scalar(Select(Chat).filter(Chat.id == chat_id))
 
@@ -220,4 +156,3 @@ async def get_chat_sessions(
         return MessagesListResponse[MessageSchema].model_validate(response_data)
     except Exception:
         raise HTTPException(status_code=500, detail="Internal server error")
->>>>>>> Stashed changes
