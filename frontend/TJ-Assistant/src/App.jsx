e9320df774b3efa365361e5939a53179dec98188
Estuss <skorpion0521@gmail.com>
2026-02-13 18:29:48 +0700

    Обновил шаблон для почты

--- frontend/TJ-Assistant/src/App.jsx
+++ frontend/TJ-Assistant/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Registration from "./components/Registration.jsx";
-import VerifyEmail from "./components/VerifyEmail.jsx";
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(() => {
